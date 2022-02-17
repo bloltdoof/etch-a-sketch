1155dcc6565d22ebca6b6104482f2a4a09c51197
Safak <eroglusafak24@gmail.com>
2022-02-17 14:32:00 +0300

    commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const grid = document.getElementById('grid');
 
 function createGrid(size) {
-    let gridSşize = size * size;
-    for (let i = 0; i < gridSşize; i++) {
+    let gridSize = size * size;
+    for (let i = 0; i < gridSize; i++) {
         let div = document.createElement('div');
         div.classList.add('grid-item');
         grid.appendChild(div);
