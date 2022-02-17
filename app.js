acefd89152dae1c68a99eb3a6e18f9c83c5ec16b
Safak <eroglusafak24@gmail.com>
2022-02-17 14:31:28 +0300

    commit

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-const reset = document.querySelector('.reset');
-const black = document.querySelector('.black');
-const rainbow = document.querySelector('.rainbow');
-const colorPicker = document.querySelector('.color-picker');
-const eraser = document.querySelector('.eraser');
-const gridContainer = document.getElementById('grid-container');
+const grid = document.getElementById('grid');
 
-
-// Function to create grid squares by row*column by the size of 10px and append to grid-container.
-
-const createGrid = (row, column) => {
-    for (let i = 0; i < row; i++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('r');
-        gridContainer.appendChild(rowDiv);
-        for (let j = 0; j < column; j++) {
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('c');
-        rowDiv.appendChild(columnDiv);
-        }
+function createGrid(size) {
+    let gridSşize = size * size;
+    for (let i = 0; i < gridSşize; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-item');
+        grid.appendChild(div);
     }
+
 }
 
-createGrid(16, 16);
+createGrid(32);
